d0969a9d48d617b5ee981a77fb33ad381b3ace51
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2020-08-14 13:50:54 +0200

    Add completed version of reducers

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import rootReducer from './reducers/todos.js';
+import rootReducer from './reducers';
 import App from './App';
 
 const store = createStore(rootReducer);
